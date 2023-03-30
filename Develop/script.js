f9b8285d2941fe8814424ee54dfef585b569f61c
seantamturk <128218653+seantamturk@users.noreply.github.com>
2023-03-29 17:37:56 -0700

    added basic psuedocode, started working on prmopt box

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,46 @@
+// on click give prompt "password length"
+  // enter desired number (8-128)
+
+// confirm box "do you want lowercase"
+  //on okay, add lowercase char to the password
+  //on cancel do not add
+
+// confirm box "do you want uppercase"
+  //on okay, add uppercase char to the password
+  //on cancel do not add
+
+// confirm box "do you want numeric"
+  //on okay, add numeric char to the password
+  //on cancel do not add 
+  
+// confirm box "do you want special char"
+  //on okay, add special char to the password
+  //on cancel do not add 
+
+// take all desired char types and add them to an array
+
+// create an equation that creates a random char at the desired length and char types
+function password(){
+  // Save the return value from the prompts
+  var charLengthPrompt =prompt("How many charcters do you want in your password? Enter a number 8-128.");
+  if (!(charLengthPrompt >= 8 && charLengthPrompt <= 128)){
+    return password();
+    }
+  // Password length
+  var length = ;
+  
+  // Password preferences (0 for no, 1 for yes)
+  var lowerCase = 0;
+  var upperCase = 0;
+  var numeric = 0;
+  var special = 0;
+  
+
+}
+
+
+
+
 // Assignment code here
 
 
@@ -8,7 +51,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 
 }
