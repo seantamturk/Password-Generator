ddfb2331bb7cbcef76b6700752a839127faf1ceb
seantamturk <128218653+seantamturk@users.noreply.github.com>
2023-03-30 22:43:59 -0700

    got the stupid password to actually write after clicking the button. I spent more time linking it to the starter code than I spent wring the generator function

--- Develop/script.js
+++ Develop/script.js
@@ -1,25 +1,27 @@
 // on click give prompt "password length"
-  // enter desired number (8-128)
+// enter desired number (8-128)
 
 // confirm box "do you want lowercase"
-  //on okay, add lowercase char to the password
-  //on cancel do not add
+//on okay, add lowercase char to the password
+//on cancel do not add
 
 // confirm box "do you want uppercase"
-  //on okay, add uppercase char to the password
-  //on cancel do not add
+//on okay, add uppercase char to the password
+//on cancel do not add
 
 // confirm box "do you want numeric"
-  //on okay, add numeric char to the password
-  //on cancel do not add 
-  
+//on okay, add numeric char to the password
+//on cancel do not add 
+
 // confirm box "do you want special char"
-  //on okay, add special char to the password
-  //on cancel do not add 
+//on okay, add special char to the password
+//on cancel do not add 
 
 // take all desired char types and add them to an array
 
 // create an equation that creates a random char at the desired length and char types
+generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", passwordgenerator);
 function passwordgenerator(){
   
   // Password length
@@ -79,23 +81,13 @@ for (var i = 0; i < length; i++){
 console.log("Your password is: " + password);
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+  function myWritePassword() {
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+
+  }
+  myWritePassword()
 }
 
 
 
-
-
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  
-  passwordText.value = password;
-  
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", passwordgenerator);
